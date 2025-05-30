059f95a74410e0f4e02cea637f17068f7cd4170f
254binaryninja <arnoldmusandu@gmail.com>
2025-05-30 21:05:26 +0300

    feat: add Custom404 component and _document workaround for build issues

---
+++ app/_document.tsx
@@ -0,0 +1,3 @@
+// This file is intentionally empty and acts as a workaround for the build error:
+// "<Html> should not be imported outside of pages/_document"
+// It should not be necessary in a proper App Router setup, but helps resolve the build issue.
