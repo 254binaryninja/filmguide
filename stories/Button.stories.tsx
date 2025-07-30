ab022ae5ad98f68866e25b17816ee21f914c7d76
254binaryninja <arnoldmusandu@gmail.com>
2025-07-30 22:22:15 +0300

    feat: Add story for Button component

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Button } from "../components/ui/button"
+
+const meta: Meta<typeof Button> = {
+  title: "Button",
+  component: Button,
+  args: {
+    variant: "default",
+    children: "Button",
+    size: "lg",
+  },
+  argTypes: {
+    variant: {
+      options: ["default", "secondary", "destructive", "outline", "ghost", "link"],
+      control: { type: "select" },
+    },
+    size: {
+      options: ["sm", "lg", "default", "icon"],
+      control: { type: "select" },
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+  render: (args) => <Button {...args} />,
+}
